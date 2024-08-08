bcc1ddc03976e42210cf3c2b8cffd358b69f60eb
William Spongberg <william.spongberg@outlook.com>
2024-08-08 14:03:14 +1000

    Created new dev branch - basic infinite procedural generation + useful assets included

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject prefab;
+
+    public float offsetX = 0.0f;
+    public float offsetY = 0.0f;
+    public float offsetZ = 0.0f;
+
+    private float playerX;
+    private float playerY;
+    private float playerZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (prefab == null)
+            prefab = Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerX = player.transform.position.x;
+        playerY = player.transform.position.y;
+        playerZ = player.transform.position.z;
+
+        prefab.transform.position = new Vector3(playerX + offsetX, playerY + offsetY, playerZ + offsetZ);
+        prefab.transform.rotation = player.transform.rotation;
+    }
+}
